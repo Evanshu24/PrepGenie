013afd398347b5518bb832a6fa3798decdb60746
Evanshu24 <evanshuj@gmail.com>
2026-07-31 01:07:09 +0530

    SiginPage frontend and basic structure of website done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../css_files/footer.css'
+
+export default function Footer(){
+    return (
+        <>
+            <footer className="footer">
+                 <p>Made by Evanshu Jain. © 2026 PrepGenie. All rights reserved.</p>
+            </footer>
+           
+        </>
+    )
+}
